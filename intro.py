5aff1746d795a75ea93afce3e744b01c360e75f8
bcoates280 <67936506+bcoates280@users.noreply.github.com>
2020-08-09 13:18:42 -0400

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,13 @@
+#Print Hello World
+
+greeting = 'Hello'
+name = 'Brandon'
+
+
+message= f'{greeting}, {name.upper()}. Welcome!'
+
+print(help(str.lower))
+
+import my_module
+
+courses = ['History', 'Math', 'Physics', 'Compsci']
